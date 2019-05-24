2b40ea402f90c1bb09f740a20496715b5ad52d6c
siberry <siberry226@gmail.com>
2019-05-24 15:51:30 -0400

    formatting

--- src/Home.js
+++ src/Home.js
@@ -4,73 +4,71 @@ import { Dropdown } from 'semantic-ui-react'
 import { COUNTRY_OPTIONS } from './countriesData.js';
 
 class Home extends React.Component {
-    state = {
+  state = {
 
-    }
+  }
 
-    handleClick = () => {
-        this.setState({
-            active: true
-        })
-    }
+  handleClick = () => {
+      this.setState({
+          active: true
+      })
+  }
 
-    render(){
-      console.log(COUNTRY_OPTIONS)
+  render(){
+    const options = [
+        { key: 'all', text: 'All', value: 'all' },
+        { key: 'continents', text: 'Continents', value: 'continents' },
+        { key: 'countries', text: 'Countries', value: 'countries' },
+      ]
 
-        const options = [
-            { key: 'all', text: 'All', value: 'all' },
-            { key: 'continents', text: 'Continents', value: 'continents' },
-            { key: 'countries', text: 'Countries', value: 'countries' },
-          ]
+    const { active } = this.state
 
-        const { active } = this.state
+    return(
+        <div>
+            {/* <h1>Welcome to the Travel App</h1> */}
+            <div className='HeaderSpacing'>
+                <div className='HeaderText'>
+                    <Header as='h1' dividing>
+                        This is Home Page
 
-        return(
-            <div>
-                {/* <h1>Welcome to the Travel App</h1> */}
-                <div className='HeaderSpacing'>
-                    <div className='HeaderText'>
-                        <Header as='h1' dividing>
-                            This is Home Page
-
-                            <div className='Sub'>
-                                <Header sub>Travel</Header>
-                            </div>
-                        </Header>
-                    </div>
-                </div>
-
-                <div className='Search'>
-                    <Input type='text' placeholder='Search...' action>
-                         <input />
-                         <Select options={options} defaultValue='all' />
-                         <Button type='submit'>Search</Button>
-                    </Input>
-                </div>
-
-                <div className='Search'>
-                    <Dropdown
-                        placeholder='Select Country'
-                        fluid
-                        search
-                        selection
-                        options={COUNTRY_OPTIONS}
-                    />
-                </div>
-
-                <div className='VideoPadding'>
-                    <Embed
-                        active={active}
-                        icon='arrow circle down'
-                        id='cAfcyrqknEE'
-                        source='youtube'
-                    />
-                    <Divider hidden />
-                    <Button content='Activate' icon onClick={this.handleClick} />
+                        <div className='Sub'>
+                            <Header sub>Travel</Header>
+                        </div>
+                    </Header>
                 </div>
             </div>
-        )
-    }
+
+            <div className='Search'>
+                <Input type='text' placeholder='Search...' action>
+                     <input />
+                     <Select options={options} defaultValue='all' />
+                     <Button type='submit'>Search</Button>
+                </Input>
+            </div>
+
+            <div className='Search'>
+                <Dropdown
+                    placeholder='Select Country'
+                    fluid
+                    search
+                    selection
+                    options={COUNTRY_OPTIONS}
+                />
+            </div>
+
+            <div className='VideoPadding'>
+                <Embed
+                    active={active}
+                    icon='arrow circle down'
+                    id='cAfcyrqknEE'
+                    source='youtube'
+                />
+                <Divider hidden />
+                <Button content='Activate' icon onClick={this.handleClick} />
+            </div>
+        </div>
+    )
+  }
 }
 
 export default Home
