64172c1d9df91fc7734784a076562f5c3c352661
siberry <siberry226@gmail.com>
2019-05-29 12:35:09 -0400

    Display page

--- src/DisplayPage.js
+++ src/DisplayPage.js
@@ -1,16 +1,42 @@
 import React from 'react'
-import { Container, Image } from "semantic-ui-react";
+import { Container, Image, Header } from "semantic-ui-react";
 
-const DisplayPage = (props) => {
-    console.log(props)
-    return(
-        <div>
-            <Container>
-                <h1>This is where the title should be: {props.name}</h1>
-                <Image src={props.image_url} ui={false}/>
-            </Container>
-        </div>
-    )
+class DisplayPage extends React.Component {
+    state= {
+      specificWebcam: null
+    }
+
+    componentDidMount() {
+      fetch(`http://localhost:3001/api/v1/webcams/${this.props.match.params.id}`)
+      .then(res => res.json())
+      .then(webcam => this.setState({
+        specificWebcam: webcam
+      }))
+    }
+
+    render() {
+      console.log(this.state.specificWebcam)
+      return(
+        <React.Fragment >
+          { this.state.specificWebcam ?
+            <div>
+              <Container>
+                <div className='Display'>
+                  <div className='DisplayHeader'>
+                    <Header as='h2' icon textAlign='center'>
+                      {this.state.specificWebcam.title}
+                    </Header>
+                  </div>
+                  <Image src={this.state.specificWebcam.img_url} ui={false}/>
+                </div>
+              </Container>
+            </div>
+            :
+            <h1>Loading</h1>
+          }
+        </React.Fragment>
+      )
+    }
 }
 
 export default DisplayPage
